c9ea3587e3fa96c7bfa92ef48088a2312fda3d47
ahmadreza0012 <zareahmadreza12@gmail.com>
2026-04-15 21:40:32 +0330

    change

--- api/proxy.js
+++ api/proxy.js
@@ -2,10 +2,11 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
   const proxy = createProxyMiddleware({
-    target: 'https://server.net-home.space',
+    target: 'https://149.104.79.165', // استفاده مستقیم از IP
     changeOrigin: true,
     ws: true,
     pathRewrite: { '^/net': '' },
+    secure: false, // اضافه شد: چون IP هست نه دامنه
     onProxyReq: (proxyReq) => {
       proxyReq.setHeader('Host', 'server.net-home.space');
     }
